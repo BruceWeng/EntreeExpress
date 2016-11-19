070671816225c21b875af58bc2c6ca0e543e9f31
BruceWeng <weng8916@gmail.com>
2016-11-18 22:59:01 -0800

    Init virtualenv

---
+++ myvirtualenv/EntreeExpress/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/bruceweng/Documents/BruceWeng/EntreeExpress/myvirtualenv/EntreeExpress/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
